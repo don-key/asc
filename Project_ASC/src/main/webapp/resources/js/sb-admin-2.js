a137ac9e2ae56a64d2f4baf369a0ac9f0749e123
dh <rlaehdzlsla@gmail.com>
2017-05-24 13:14:45 +0900

    수정

--- Project_ASC/src/main/webapp/resources/js/sb-admin-2.js
+++ Project_ASC/src/main/webapp/resources/js/sb-admin-2.js
@@ -7,7 +7,7 @@ $(function() {
     $('#side-menu').metisMenu();
 });
 
-//Loads the correct sidebar on window load,
+//Loads the correct sidebar on window load, 
 //collapses the sidebar on window resize.
 // Sets the min-height of #page-wrapper to window size
 var height = ((this.window.innerHeight > 0) ? this.window.innerHeight : this.screen.height) - 1;

9cf3da9251898d8e7c0b294bd6f9420d43533181
jongyoon <jongyoon13@hanmail.net>
2017-05-31 15:14:12 +0900

    자료실 관련 도메인

--- Project_ASC/src/main/java/com/object/asc/project/domain/Library.java
+++ Project_ASC/src/main/java/com/object/asc/project/domain/Library.java
@@ -1,5 +1,34 @@
 package com.object.asc.project.domain;
 
 public class Library {
-
+	private int libraryNo;
+	private int projectListNo;
+	
+	public Library() {}
+	
+	public Library(int libraryNo, int projectListNo) {
+		this.libraryNo = libraryNo;
+		this.projectListNo = projectListNo;
+	}
+	
+	public int getLibraryNo() {
+		return libraryNo;
+	}
+	
+	public void setLibraryNo(int libraryNo) {
+		this.libraryNo = libraryNo;
+	}
+	
+	public int getProjectListNo() {
+		return projectListNo;
+	}
+	
+	public void setProjectListNo(int projectListNo) {
+		this.projectListNo = projectListNo;
+	}
+	
+	@Override
+	public String toString() {
+		return "Library [libraryNo=" + libraryNo + ", projectListNo=" + projectListNo + "]";
+	}
 }
--- Project_ASC/src/main/java/com/object/asc/project/domain/LibraryList.java
+++ Project_ASC/src/main/java/com/object/asc/project/domain/LibraryList.java
@@ -1,5 +1,78 @@
 package com.object.asc.project.domain;
 
+import java.util.Date;
+
 public class LibraryList {
+	private int libraryListNo;
+	private int libraryNo;
+	private int userNo;
+	private String title;
+	private String fileName;
+	private Date regdate;
+
+	public LibraryList() {}
+
+	public LibraryList(int libraryListNo, int libraryNo, int userNo, String title, String fileName, Date regdate) {
+		this.libraryListNo = libraryListNo;
+		this.libraryNo = libraryNo;
+		this.userNo = userNo;
+		this.title = title;
+		this.fileName = fileName;
+		this.regdate = regdate;
+	}
+
+	public int getLibraryListNo() {
+		return libraryListNo;
+	}
+
+	public void setLibraryListNo(int libraryListNo) {
+		this.libraryListNo = libraryListNo;
+	}
+
+	public int getLibraryNo() {
+		return libraryNo;
+	}
+
+	public void setLibraryNo(int libraryNo) {
+		this.libraryNo = libraryNo;
+	}
+
+	public int getUserNo() {
+		return userNo;
+	}
+
+	public void setUserNo(int userNo) {
+		this.userNo = userNo;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getFileName() {
+		return fileName;
+	}
+
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+
+	public Date getRegdate() {
+		return regdate;
+	}
+
+	public void setRegdate(Date regdate) {
+		this.regdate = regdate;
+	}
+
+	@Override
+	public String toString() {
+		return "LibraryList [libraryListNo=" + libraryListNo + ", libraryNo=" + libraryNo + ", userNo=" + userNo
+				+ ", title=" + title + ", fileName=" + fileName + ", regdate=" + regdate + "]";
+	}
 
 }
